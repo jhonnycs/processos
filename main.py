642968e01e6ad6cc1abb4fbe50a9cafcc4ef7ed3
Lucas-Delacroix <delacroixdev@gmail.com>
2025-10-02 20:32:24 -0300

    feat: Ajusta main

--- main.py
+++ main.py
@@ -4,33 +4,39 @@ from algorithms.round_robin import round_robin
 from load_json import JsonData
 from models.Metricas import Metricas
 
-a = JsonData("c.json")
-metricas = Metricas()
+def main(json):
+    a = JsonData(json)
+    metricas = Metricas()
 
-results_fcfs = fcfs(a.processes, a.context_switch_cost)
-results_sjf = sjf(a.processes, a.context_switch_cost)
+    results_fcfs = fcfs(a.processes, a.context_switch_cost)
+    results_sjf = sjf(a.processes, a.context_switch_cost)
 
-metricas.adicionar("FCFS" ,results_fcfs)
-metricas.adicionar("SJF",results_sjf)
+    metricas.adicionar("FCFS" ,results_fcfs)
+    metricas.adicionar("SJF",results_sjf)
 
-results_rr = []
-for q in a.rr_quantums:
-    results_rr.append(round_robin(a.processes, a.context_switch_cost, q))
+    results_rr = []
+    for q in a.rr_quantums:
+        results_rr.append(round_robin(a.processes, a.context_switch_cost, q))
 
-for result in results_rr:
-    metricas.adicionar(f"RR - {result.quantum}", result)
+    for result in results_rr:
+        metricas.adicionar(f"RR - {result.quantum}", result)
 
-# metricas.plot_tempos_espera()
-# metricas.plot_tempos_retorno()
-# metricas.plot_vazao()
-# metricas.plot_tempos_espera()
-# metricas.plot_tempos_retorno()
-# metricas.plot_vazao()
+    # metricas.plot_tempos_espera()
+    # metricas.plot_tempos_retorno()
+    # metricas.plot_vazao()
+    # metricas.plot_tempos_espera()
+    # metricas.plot_tempos_retorno()
+    # metricas.plot_vazao()
 
-results_fcfs.plot_gantt_classic()
-results_sjf.plot_gantt_classic()
-results_fcfs.show_timeline()
+    results_fcfs.plot_gantt_classic()
+    results_sjf.plot_gantt_classic()
+    results_fcfs.show_timeline()
 
-for result in results_rr:
-    result.plot_gantt_classic()
+    for result in results_rr:
+        result.plot_gantt_classic()
 
+
+main("c.json")
+main("d.json")
+main("e.json")
+main("f.json")
